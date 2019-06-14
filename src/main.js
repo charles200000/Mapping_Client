0d5d76740cd176ec9ce8b6bf902545e4278cf0bd
Charles20000 <charles.farhat@hotmail.fr>
2019-06-15 01:42:21 +0200

    started project
    basic map integration

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var L = require('leaflet');
+var map = L.map('map').setView([45.528, -122.680], 13);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
